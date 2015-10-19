00d8f1299064484b60a71b821a1b59fc6d5b5a41
4johndoe <bogdandemchenko@gmail.com>
2015-10-19 15:45:12 +0300

    day 4 ends

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -17,25 +17,42 @@ class Email extends CI_Controller
 
 	function send()
 	{
-		$this->load->library('email'); //, $config
-		$this->email->set_newline("\r\n");
-		
-		$this->email->from('mailbox4johndoe@gmail.com', 'John Doe');
-		$this->email->to('bogdandemchenko@gmail.com');
-		$this->email->subject('This is an email test');
-		$this->email->message('It\'s working. Yeah!');
+		$this->load->library('form_validation');
 
-		$path = $this->config->item('server_root');
-		$file = $path . '/ci/attachments/info.txt';
+		//rules
+		$this->form_validation->set_rules('name', 'Name', 'trim|required');
+		$this->form_validation->set_rules('email', 'Email Address', 'trim|required|valid_email');
 
-		$this->email->attach($file);
-
-
-		if ($this->email->send()) 
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('newsletter');
+		} else
 		{
-			echo('Your email was send, fool.');
-		} else {
-			show_error($this->email->print_debugger());
+			// has passed
+			$name = $this->input->post('name');
+			$email = $this->input->post('email');
+
+			//send
+			$this->load->library('email'); //, $config
+			$this->email->set_newline("\r\n");
+			
+			$this->email->from('mailbox4johndoe@gmail.com', 'John Doe');
+			$this->email->to($email);
+			$this->email->subject('Test signUp info');
+			$this->email->message('You\'ve now signed up.');
+
+			$path = $this->config->item('server_root');
+			$file = $path . '/ci/attachments/info.txt';
+
+			$this->email->attach($file);
+
+
+			if ($this->email->send()) 
+			{
+				// echo('Your email was send, fool.');
+				$this->load->view('signup_confirmation_view');
+			} else {
+				show_error($this->email->print_debugger());
+			}
 		}
 	}
 }
