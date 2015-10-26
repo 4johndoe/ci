55f09f2c461dff12f3a311a58645cd10e74c34d2
4johndoe <bogdandemchenko@gmail.com>
2015-10-26 16:34:15 +0200

    5 5

---
+++ application/controllers/login.php
@@ -0,0 +1,9 @@
+<?php 
+
+class Login extend CI_Controller
+{
+	function index()
+	{
+		
+	}
+}
--- application/views/home.php
+++ application/views/home.php
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title></title>
-</head>
-<body>
-	<div id="container">
-		<p>My view has been loaded</p>
-
-		<?php foreach ($rows as $r) { ?>
-			<h1><?php echo $r->title; ?></h1>
-			<div><?php echo $r->contents; ?></div>
-			<!-- <div><a href="#"><?php echo $r->author; ?></a></div> -->
-		<?php } ?>
-	</div>
-</body>
-</html>
+nothing
