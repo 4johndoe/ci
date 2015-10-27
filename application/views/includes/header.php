578d713a09aa1e6fcdea8caf3793b5eb8d009558
4johndoe <bogdandemchenko@gmail.com>
2015-10-27 16:17:16 +0200

    6 36

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -3,9 +3,6 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="<?php base_url(); ?>css/style.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css">
 </head>
 <body>
-	
-</body>
-</html>
