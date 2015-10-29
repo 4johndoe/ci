31248fc5a8a590ba80fead281816651da0493780
4johndoe <bogdandemchenko@gmail.com>
2015-10-29 11:13:17 +0200

    reborn bitch

--- application/config/config.php
+++ application/config/config.php
@@ -19,8 +19,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = 'http://localhost/ci/';
 
-$config['server_root'] = $_SERVER['DOCUMENT_ROOT'];
-
 /*
 |--------------------------------------------------------------------------
 | Index File
