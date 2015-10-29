678808773795128567437a6532bfdc6539db9263
4johndoe <bogdandemchenko@gmail.com>
2015-10-29 12:14:18 +0200

    style's back

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,1 +1,11 @@
-Login.php
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller 
+{
+
+	public function index()
+	{
+		// $this->load->view('welcome_message');
+	}
+}
