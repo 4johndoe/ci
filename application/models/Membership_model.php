d0507e6a9ab70422af0a487e7652b1a145c7e392
4johndoe <bogdandemchenko@gmail.com>
2015-10-30 16:13:41 +0200

    46

--- application/models/Membership_model.php
+++ application/models/Membership_model.php
@@ -14,4 +14,18 @@ class Membership_model extends CI_Model
 		}
 		
 	}
+
+	function create_member()
+	{
+		$new_member_insert_data = array(
+			'first_name' => $this->input->post('first_name'),
+			'last_name' => $this->input->post('last_name'),
+			'email_address' => $this->input->post('email_address'),
+			'username' => $this->input->post('username'),
+			'password' => md5($this->input->post('password'))
+		);
+
+		$insert = $this->db->insert('membership', $new_member_insert_data);
+		return $insert;
+	}
 }
